53d01036ec0e63ad055fb00f3eef3a2d8d600c69
usarekoski <ukonvaaja10@gmail.com>
2015-08-18 14:12:59 +0300

    more specs

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,9 +4,14 @@ describe Game do
 
   let(:game) { Game.new(false) }
 
+  before :each do
+    @player = double("Player", name: "Name", symbol: "X")
+  end
+
   it "should display welcome message" do
     expect($stdout).to receive(:puts).with(/Tic-tac-toe/)
     game.welcome
   end
 
 end
+
